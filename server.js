5648727da42a60f17b76618d0d6b12fb6f68bb12
Artur Wagner <arturwagner99@hotmail.com>
2026-04-18 11:26:48 +0100

    teste

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream(path.join(__dirname, "index.html")).pipe(res);
+});
+
+server.listen(3000, () => console.log("Running on port 3000"));
